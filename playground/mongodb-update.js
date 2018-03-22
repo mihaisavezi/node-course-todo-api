61b0d8386f26afe19a995f6d8eb304fb775bdb9c
Mihai Popescu <mihai.popescu@LBUO0010-8.local>
2018-03-22 11:29:56 +0200

    add /POST status

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,33 +7,33 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB server');
 
-    db.collection('Todos').findOneAndUpdate({
-        _id: new ObjectID('5ab366b9f16ea3e388300e9c')
-    }, {
-        $set: {
-            completed: true
-        }
-    }, {
-        returnOriginal: false
-    }).then((result) => {
-            console.log(result);
-        })
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5ab366b9f16ea3e388300e9c')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //         console.log(result);
+    //     })
 
 
-    db.collection('Users').findOneAndUpdate({
-        _id: new ObjectID('5ab36ad4f16ea3e3883011b2')
-    }, {
-        $set: {
-            name: 'Mihai'
-        },
-        $inc: {
-            age: 1
-        }
-    }, {
-        returnOriginal: false
-    }).then((result) => {
-            console.log(result);
-        })
+    // db.collection('Users').findOneAndUpdate({
+    //     _id: new ObjectID('5ab36ad4f16ea3e3883011b2')
+    // }, {
+    //     $set: {
+    //         name: 'Mihai'
+    //     },
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //         console.log(result);
+    //     })
 
 
     // db.close() // close connection with mongodbserver
