3adbdf10096745123b4e40362b6a226647f37084
MIHAI POPESCU <mihai.ppscu@gmail.com>
2018-03-24 18:28:30 +0200

    add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -320,3 +320,27 @@ describe('POST /users/login', () => {
             })
     })
 })
+
+describe('DELETE /users/me/token', () => {
+    // when valid x-auth token is passed, user gets logged out
+    it('should remove auth token on logout', (done) => {
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id)
+                    .then(user => {
+                        expect(user.tokens.length).toBe(0);
+
+                        done();
+                    })
+                    .catch(e => done(e))
+            })
+    });
+})
