6f872a1403d6e757e6be140c5a0f251a71b416b2
Mihai Popescu <mihai.popescu@LBUO0010-8.local>
2018-03-24 12:59:20 +0200

    hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 
 var UserSchema = new mongoose.Schema({
@@ -75,6 +76,20 @@ UserSchema.statics.findByToken = function (token) {
     });
   };
 
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if(user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hashedResult) => {
+                user.password = hashedResult;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
 
 const User = mongoose.model('User', UserSchema);
 
